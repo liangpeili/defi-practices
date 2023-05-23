cec9bf104f53473ff0c8d648e46f202604e21495
liangpeili <liangpeili@foxmail.com>
2023-05-23 14:23:29 +0800

    add comment

--- defi-practice/contracts/02_PriceFeed.sol
+++ defi-practice/contracts/02_PriceFeed.sol
@@ -7,6 +7,7 @@ contract PriceFeed {
     /**
      * Returns the latest price.
      */
+    // 抵押品的价格，比如1 LPL = 2 USD
     function getLatestPrice() public pure returns (int price) {
         return 2 * 1e18;
     }
